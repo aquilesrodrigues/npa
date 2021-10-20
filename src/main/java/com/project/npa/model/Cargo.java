256d1e0ed20885d003d2dd14b27fcf611a615923
ADELINO SANTOS <afsjr.bh@gmail.com>
2021-10-20 15:58:59 -0300

    criado model Cargo

--- src/main/java/com/project/npa/model/Cargo.java
+++ src/main/java/com/project/npa/model/Cargo.java
@@ -1,30 +1,24 @@
 package com.project.npa.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 
 @Entity
 @Table(name="Departamento")
 @Getter
 @Setter
-public class Departamento {
+public class Cargo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @NotNull
-    private String nome;
+    private String id;
 
     @Lob
-    private String descricao;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
-    private LocalDate date;
+    @NotNull
+    private String nome;
 
 }
