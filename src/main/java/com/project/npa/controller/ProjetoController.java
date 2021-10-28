a773cd2a44f5f9775618604787337b0fe981b275
Aquiles Rodrigues <cyberfazer@gmail.com>
2021-10-28 09:18:06 -0300

    Create Listar Projeto

--- src/main/java/com/project/npa/controller/ProjetoController.java
+++ src/main/java/com/project/npa/controller/ProjetoController.java
@@ -1,26 +1,24 @@
 package com.project.npa.controller;
-
-
 import com.project.npa.exception.CentroCustoException;
 import com.project.npa.exception.FuncionarioException;
 import com.project.npa.model.CentroCusto;
 import com.project.npa.model.Funcionario;
 import com.project.npa.model.Projeto;
+import com.project.npa.model.dto.CentroCustoDTO;
 import com.project.npa.model.dto.ProjetoSimplesDTO;
 import com.project.npa.repository.CentroCustoRepository;
 import com.project.npa.repository.FuncionarioRepository;
 import com.project.npa.repository.ProjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 public class ProjetoController {
@@ -54,5 +52,9 @@ public class ProjetoController {
         return new ResponseEntity<ProjetoSimplesDTO>(new ProjetoSimplesDTO(projetoCadastro), HttpStatus.CREATED);
     }
 
+    @GetMapping(URLBASE)
+    public ResponseEntity<List<ProjetoSimplesDTO>> listar() {
+        List<>
+    }
 
 }
