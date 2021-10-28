f009f75c268fada1dd5087a7dec94a2d5f97a540
Aquiles Rodrigues <cyberfazer@gmail.com>
2021-10-27 22:13:48 -0300

    Update Notation @GeneratedValue

--- src/main/java/com/project/npa/model/Projeto.java
+++ src/main/java/com/project/npa/model/Projeto.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 public class Projeto {
 
     @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false, length = 50)
